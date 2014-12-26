c1fe07b231043dcc9568de78ac3534f264b091ff
Don Park <donpark@docuverse.com>
2014-12-26 04:20:32 -0800

    make config overridable

--- js/util/config.js
+++ js/util/config.js
@@ -1,8 +1,12 @@
 'use strict';
 
-module.exports = {
-    HTTP_URL: 'http://a.tiles.mapbox.com/v4',
-    HTTPS_URL: 'https://a.tiles.mapbox.com/v4',
-    FORCE_HTTPS: false,
-    REQUIRE_ACCESS_TOKEN: true
-};
+if (typeof window !== 'undefined' && typeof window.mapboxglConfig === 'object') {
+    module.exports = window.mapboxglConfig;
+} else {
+    module.exports = {
+        HTTP_URL: 'http://a.tiles.mapbox.com/v4',
+        HTTPS_URL: 'https://a.tiles.mapbox.com/v4',
+        FORCE_HTTPS: false,
+        REQUIRE_ACCESS_TOKEN: true
+    };
+}
